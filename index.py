a88d215b772eefd2b48de50094998fdd1d7e9b1d
Dagony <dagony@gmail.com>
2020-11-10 10:06:07 +0100

    Opdracht 1

---
+++ index.py
@@ -0,0 +1,23 @@
+from random import randrange
+
+# Implementeer je de lijst
+thislist = ["Henk", "Sjaak", "Bier", "Pubquiz", "Testcoders", "Scheissarbeid", "Koekenpan", "Word", "Wak", "Vet",
+            "Leip", "Rona", "Koningsdag?", "Wintersport?", "Urenapp", "Googledag"]
+
+# Maak logica om de MagicWord te selecteren
+randomIndex = randrange(0, len(thislist) - 1)
+
+MagicWord = thislist[randomIndex]
+
+# Maak logica waar de gebruiker input kan leveren, bv in de console.
+# Zorg dat de gebuiker bv 5 pogingen heeft
+good_input = None
+attempts = 0
+while not good_input and attempts < 5:
+    playersGuess = input()
+    if playersGuess != MagicWord:
+        attempts = attempts + 1
+        print('Probeer het nog een keer!')
+    else:
+        good_input = playersGuess
+        print('Geraden! Gefeliciteerd!')
